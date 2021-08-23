9314e444b7c641294553966148c5cebacde569f1
DevShape <42915957+DevShape@users.noreply.github.com>
2021-08-23 22:28:02 +0200

    Add User Weapon choice

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -3,6 +3,24 @@ import kotlin.random.Random
 fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Random.nextInt(optionsParam.size))]
+
+fun getUserChoice(optionsParam: Array<String>) {
+    var x = "Please enter one of the following: "
+    for (item in optionsParam) {
+        x += "$item, "
+    }
+    x = x.dropLast(2)
+    x += "."
+    println(x)
+    while (true) {
+        val userInput = readLine()
+        if (userInput != null && userInput in optionsParam) {
+            break
+        } else
+            println("Please try again")
+    }
+}
